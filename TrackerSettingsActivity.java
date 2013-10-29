efb3afcf7342f5d5e90e2559ffefb3da2b4d6337
ja-zg <edjber@hotmail.com>
2013-10-29 16:26:32 +0100

    Update TrackerSettingsActivity.java

--- TrackerSettingsActivity.java
+++ TrackerSettingsActivity.java
@@ -2,17 +2,18 @@ package hr.hyperactive.tracker;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class TrackerSettingsActivity extends Activity {	
 	PhoneStateBroadcastReciever prefs;
-	int TIME_DELAY = 5000;
 	Handler handler;
+	
+	EditText timerDelay;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,33 +21,34 @@ public class TrackerSettingsActivity extends Activity {
 		
 		setContentView(R.layout.tracker_settings);
 		
-		EditText timerDelay = (EditText) findViewById(R.id.editText_Set_Timer);
-		String text = timerDelay.getText().toString();
-		int time = Integer.parseInt(text);
-	
+		timerDelay = (EditText) findViewById(R.id.editText_Set_Timer);
+			
 		Intent intent = new Intent(this, TrackerService.class);
 		startService(intent);
-		Toast.makeText(getApplicationContext(), "Service se upalio", Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), "paljenje servicea", Toast.LENGTH_LONG).show();
 		
 	}
 	
 	public void click(View v) {
+		String text = timerDelay.getText().toString();
+		int TIME_DELAY = 0;
+		try {
+			TIME_DELAY = Integer.parseInt(text) * 1000;
+		} catch(NumberFormatException e) {
+			Log.e("TAG", e.toString());
+		}
+		
+		timerDelay.setTextColor(getResources().getColor(R.color.grey));
+		
 		handler = new Handler();
 		handler.postDelayed(new Runnable() {
 			
 			@Override
 			public void run() {
-				Toast.makeText(getApplicationContext(), "Toast poruka", Toast.LENGTH_LONG).show();		
+				Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_LONG).show();				
 			}
 		}, TIME_DELAY);
-	}
-	
-	private void updateDataLocally() {
-		SharedPreferences m_sharedPrefs = getSharedPreferences("MyPrefs", 0);
-		String number = m_sharedPrefs.getString(prefs.PREFS_NUMBER, "1");
-		String startTime = m_sharedPrefs.getString(prefs.PREFS_START_TIME, "2");
-		String endTime = m_sharedPrefs.getString(prefs.PREFS_END_TIME, "3");
-		Toast.makeText(getApplicationContext(), number + " " + startTime + " " + endTime, Toast.LENGTH_LONG).show();
+		
 	}
 	
 }
